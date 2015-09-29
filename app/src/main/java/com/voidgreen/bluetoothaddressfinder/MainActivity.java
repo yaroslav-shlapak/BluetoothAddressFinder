d245bcf44871a134f36f0fa8c4de40d8d168e682
yaroslav <yaroslav.shlapak@gmail.com>
2015-09-29 23:53:17 +0300

    Save

--- app/src/main/java/com/voidgreen/bluetoothaddressfinder/MainActivity.java
+++ app/src/main/java/com/voidgreen/bluetoothaddressfinder/MainActivity.java
@@ -9,29 +9,51 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     int REQUEST_ENABLE_BT = 666;
     List mArrayList = new ArrayList();
+    HashMap<String, String> map = new HashMap<>();
+    BluetoothAdapter mBluetoothAdapter;
     ArrayAdapter<String> adapter;
+    ArrayAdapter mapAdapter;
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             // When discovery finds a device
+            Log.d("bluetooth.log", action);
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // Get the BluetoothDevice object from the Intent
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // Add the name and address to an array adapter to show in a ListView
-
-                mArrayList.add(device.getName() + "\t" + device.getAddress());
+                String name = device.getName();
+                String address = device.getAddress();
+                if(name == null) {
+                    name  = "undefined";
+                }
+                String s = name + "\t" + address;
+                mArrayList.add(s);
+                map.put(address, name);
+                Log.d("bluetooth.log", s);
                 adapter.notifyDataSetChanged();
+                mapAdapter.notifyDataSetChanged();
+
 
             }
         }
@@ -42,17 +64,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
         }
 
-/*
+
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
-*/
+
 
 
     }
@@ -72,12 +94,17 @@ public class MainActivity extends AppCompatActivity {
 
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+        mBluetoothAdapter.startDiscovery();
+        mArrayList = new ArrayList<>(map.keySet());
 
-        adapter = new ArrayAdapter<String>(this,
+        adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, mArrayList);
 
+
+        map.put("MAC", "NAME");
+        mapAdapter = new HashMapArrayAdapter(this, R.layout.my_adapter_layout, new ArrayList(map.entrySet()));
         ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        listView.setAdapter(mapAdapter);
     }
 
     @Override
@@ -107,3 +134,80 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+class HashMapArrayAdapter extends ArrayAdapter {
+
+    private static class ViewHolder {
+        TextView tV1;
+        TextView tV2;
+    }
+
+    public HashMapArrayAdapter(Context context, int textViewResourceId, List<Map.Entry<String, String>> objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.my_adapter_layout, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tV1 = (TextView) convertView.findViewById(android.R.id.text1);
+            viewHolder.tV2 = (TextView) convertView.findViewById(android.R.id.text2);
+            convertView.setTag(viewHolder);
+        } else
+            viewHolder = (ViewHolder) convertView.getTag();
+
+        Map.Entry<String, String> entry = (Map.Entry<String, String>) this.getItem(position);
+
+        viewHolder.tV1.setText(entry.getKey());
+        viewHolder.tV2.setText(entry.getValue());
+        return convertView;
+    }
+}
+
+/*class MyAdapter extends BaseAdapter {
+    private final ArrayList mData;
+
+    public MyAdapter(Map<String, String> map) {
+        mData = new ArrayList();
+        mData.addAll(map.entrySet());
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Map.Entry<String, String> getItem(int position) {
+        return (Map.Entry) mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO implement you own logic with ID
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final View result;
+
+        if (convertView == null) {
+            result = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_adapter_layout, parent, false);
+        } else {
+            result = convertView;
+        }
+
+        Map.Entry<String, String> item = getItem(position);
+
+        // TODO replace findViewById by ViewHolder
+        ((TextView) result.findViewById(android.R.id.text1)).setText(item.getKey());
+        ((TextView) result.findViewById(android.R.id.text2)).setText(item.getValue());
+
+        return result;
+    }
+}*/
+
